12a436b2a7b3d1e02e0d44f9206c67571a3e8a5a
Gurumadwa <gurumadwak@gmail.com>
2024-04-06 19:49:21 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(process.env.MONGO).then(()=>console.log('Mongodb connected')).catch((err)=> console.log(err))
 
 
 app.listen(3000,()=>console.log('Server started'))
